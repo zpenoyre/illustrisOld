15dd92cc6e1cc42d8e3aeef468d45c13991588f7
Zephyr Penoyre <zephyrpenoyre@gmail.com>
2017-02-03 16:34:12 +0000

    now outputs to temp.hdf5 file

--- illustrisAPI/data.py
+++ illustrisAPI/data.py
@@ -1,4 +1,6 @@
 import requests
+import h5py
+import numpy as np
 
 baseUrl = 'http://www.illustris-project.org/api/'
 headers = {"api-key":"cc4ff6392e79c9e08c158e5ae5493718"}
@@ -13,12 +15,13 @@ def get(path, params=None): #gets data from url, saves to file
     if r.headers['content-type'] == 'application/json':
         return r.json() # parse json responses automatically
 
+    tempFile='temp.hdf5'
     # Saves to file, currently disabled
     if 'content-disposition' in r.headers:
         filename = r.headers['content-disposition'].split("filename=")[1]
-        with open(filename, 'wb') as f:
+        with open(tempFile, 'wb') as f:
             f.write(r.content)
-        return 'this' # return the filename string
+        return tempFile # return the filename string
 
     return r
 
@@ -38,3 +41,16 @@ def getSub(subs,whichSub):
     sub_url=subs['results'][whichSub]['url']
     sub=get(sub_url)
     return sub
+
+def getData(sub): #at the moment only return pos and vel, would love to generalise
+    cut_req={'dm':'Coordinates,Velocities'}
+    cutout=get(sub['meta']['url']+'cutout.hdf5',cut_req)
+    
+    with h5py.File(cutout,'r') as f:
+        x = f['PartType1']['Coordinates'][:,0] - sub['pos_x'] # ckpc/h
+        y = f['PartType1']['Coordinates'][:,1] - sub['pos_y']
+        z = f['PartType1']['Coordinates'][:,2] - sub['pos_z']
+        vx = f['PartType1']['Velocities'][:,0] - sub['vel_x'] # km/s
+        vy = f['PartType1']['Velocities'][:,1] - sub['vel_y']
+        vz = f['PartType1']['Velocities'][:,2] - sub['vel_z']
+    return np.vstack((x,y,z)),np.vstack((vx,vy,vz))
